c05b7ef218a461b1585cf148f5075307275f8568
Sachleen <97253957+sachT19@users.noreply.github.com>
2022-07-13 23:16:32 +0300

    Create intOverflow.java

---
+++ intOverflow.java
@@ -0,0 +1,18 @@
+public class IntegerOverflow
+{
+    public static void main(String[] args)
+    {
+       
+        //The minimum value for an integer.
+        //The maximum value for an integer.
+        //The minimum value subtracted by 1.
+        //The maximum value with 1 added.
+        
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(Integer.MAX_VALUE + 1);
+        
+
+    }
+}
